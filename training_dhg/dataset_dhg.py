7e3deb83d7164db4d750ac0b4039435eedea31cb
Malcolm Zhao <96398778+ZhaoHanrui@users.noreply.github.com>
2025-11-25 01:06:43 -0500

    Implemented ablation for dhg, precision, and recall

--- training_dhg/dataset_dhg.py
+++ training_dhg/dataset_dhg.py
@@ -8,6 +8,7 @@ import pandas as pd
 import os
 import torch.nn.functional as F
 import dhg
+import numpy as np
 
 PATH_GRAPH = "../data/graphs/"
 PATH_HYPERGRAPH = "../data/hypergraphs/"
@@ -21,9 +22,59 @@ def normalize_graph(x: torch.Tensor) -> torch.Tensor:
     return (x - mu) / sd
 
 
+# class AbideDatasetDHG(Dataset):
+#     def __init__(self, train=True, split=0.9, split_seed=0):
+#         self.dir = PATH_HYPERGRAPH
+
+#         all_paths = glob.glob(f"{self.dir}*.pt")
+
+#         df = pd.read_csv(PATH_ABIDE_LABELS)
+#         self.id_to_label_dict = dict(zip(df["FILE_ID"], df["DX_GROUP"]))
+
+#         rng = torch.Generator().manual_seed(split_seed)
+#         perm = torch.randperm(len(all_paths), generator=rng).tolist()
+#         cutoff = int(len(all_paths) * split)
+#         chosen_idx = perm[:cutoff] if train else perm[cutoff:]
+#         self.x_paths = [all_paths[i] for i in chosen_idx]
+
+#         # Identify minimum time series length
+#         self.min_dim = float("inf")
+#         for path in all_paths: self.min_dim = min(self.min_dim, torch.load(path, weights_only=False).x.size(-1))
+
+
+#     def __len__(self):
+#         return len(self.x_paths)
+
+
+#     def __getitem__(self, idx):
+#         x_path_absolute = self.x_paths[idx]
+#         x_path_filename = os.path.basename(x_path_absolute)
+#         file_id = x_path_filename.removesuffix("_hypergraph.pt")
+
+#         x = torch.load(x_path_absolute, weights_only=False)
+#         # Truncate to the minimium time series length
+#         x.x = normalize_graph(x.x[:, : self.min_dim])
+
+#         pairs = x.edge_index.t().tolist()
+#         num_nodes = x.x.size(0)
+#         buckets = [[] for _ in range(x.num_hyperedges)]
+#         for n, h in pairs: buckets[h].append(n)
+#         hedges = [tuple(sorted(set(ns))) for ns in buckets if len(ns) > 0]
+#         hg = dhg.Hypergraph(num_nodes, hedges)
+#         y = torch.tensor(self.id_to_label_dict[file_id], dtype=torch.long)
+#         return x.x, y-1, hg
+
+
+def calc_pc(X):
+    PC = np.corrcoef(X)
+    PC = np.nan_to_num(PC, nan=0.0)
+    PC = torch.from_numpy(PC).float()
+    return PC
+
 class AbideDatasetDHG(Dataset):
-    def __init__(self, train=True, split=0.9, split_seed=0):
+    def __init__(self, train=True, split=0.9, split_seed=0, ablation=False):
         self.dir = PATH_HYPERGRAPH
+        self.ablation = ablation
 
         all_paths = glob.glob(f"{self.dir}*.pt")
 
@@ -51,15 +102,19 @@ class AbideDatasetDHG(Dataset):
         file_id = x_path_filename.removesuffix("_hypergraph.pt")
 
         x = torch.load(x_path_absolute, weights_only=False)
-        # Truncate to the minimium time series length
-        x.x = normalize_graph(x.x[:, : self.min_dim])
+        x.x = calc_pc(x.x)
+        # # Truncate to the minimium time series length
+        # x.x = normalize_graph(x.x[:, : self.min_dim])
 
         pairs = x.edge_index.t().tolist()
         num_nodes = x.x.size(0)
+
         buckets = [[] for _ in range(x.num_hyperedges)]
         for n, h in pairs: buckets[h].append(n)
         hedges = [tuple(sorted(set(ns))) for ns in buckets if len(ns) > 0]
+        if self.ablation: hedges = [h for h in hedges if len(h) <= 2]
         hg = dhg.Hypergraph(num_nodes, hedges)
+        
         y = torch.tensor(self.id_to_label_dict[file_id], dtype=torch.long)
         return x.x, y-1, hg
 
